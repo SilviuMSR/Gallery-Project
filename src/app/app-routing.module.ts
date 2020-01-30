85ca5289c5edc299f078e92a12a780699d18f374
silviumsr <manzursilviu@yahoo.com>
2020-01-30 10:25:36 +0200

    Init application, added search/metrics component, integrated bootstrap/bootswatch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SearchComponent } from './components/search/search.component';
+import { MetricsComponent } from './components/metrics/metrics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent },
+  { path: 'metrics', component: MetricsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
