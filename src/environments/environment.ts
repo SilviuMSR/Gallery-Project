6bf35fa6c5bead1cd1afc74fc70e8d1f803c0b01
silviumsr <manzursilviu@yahoo.com>
2020-02-02 11:20:17 +0200

    removed access key and environments from gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_ACCESS_KEY: '4713d51fc54cece1766505ccb2c2a794af6f1bf4c263095ea40255e0d8bb6b8d'
+  API_ACCESS_KEY: 'thisisapiaccesskey'
 };
 
 /*
